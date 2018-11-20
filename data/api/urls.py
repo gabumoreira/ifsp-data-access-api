11ade18503ef41f06b51c1f3377dc4a925c6f3c3
Gabriel Moreira <joaogmrodrigues@outlook.com>
2018-11-20 18:22:16 -0200

    Remoção de rota aninhada

--- data/api/urls.py
+++ data/api/urls.py
@@ -1,14 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from rest_framework_extensions.routers import NestedRouterMixin
 
 from . import views
 
-class NestedDefaultRouter(NestedRouterMixin, DefaultRouter):
-    """ Foo de configuração para roteamento aninhado na URL """
-    pass
-
 # Recursos diretos e listagens
-router 		= NestedDefaultRouter()
+router 		= DefaultRouter()
 router.register('areas', views.AreasViewSet)
 router.register('base', views.BaseViewSet)
 router.register('curso', views.CursoViewSet)
